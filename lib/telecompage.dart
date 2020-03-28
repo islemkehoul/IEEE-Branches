ae484afc007037aca0519e4b46745cee487b965d
islemkehoul <islemkehoul98@gmail.com>
2020-03-28 22:37:15 +0400

    first commit

---
+++ lib/telecompage.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class Telecom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
